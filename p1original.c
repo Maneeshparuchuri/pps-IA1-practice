cebee9dfc0fd257b9da3c11c427bb375b2deafa9
Maneesh1721 <94883199+Maneeshparuchuri@users.noreply.github.com>
2022-02-02 09:21:41 +0000

    p1original.c

--- p1original.c
+++ p1original.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+void input(int *a,int *b)
+{
+  printf("enter two numbers");
+  scanf("%d%d",a,b);
+}
+void find_sum (int a,int b, int *find_sum)
+{
+  *find_sum=a+b;
+}
+void output (int a, int b, int c)
+
+{
+  printf("sum%d+%d is %d\n",a,b,c);
+}
 int main()
 {
-  int a,b,c;
-  printf("enter two numbers\n");
-  scanf("%d%d",&a,&b);
-  c=a+b;
-  printf("%d + %d is %d\n",a,b,c);
-  }
+  int x,y,z;
+  input(&x,&y);
+  find_sum(x,y,&z);
+  output(x,y,z);
+  return 0;
+}
