4b748b7c1e38eb83cee5ffa286bd365a2ca4d2c8
Maneesh1721 <94883199+Maneeshparuchuri@users.noreply.github.com>
2022-02-02 09:16:45 +0000

    p2original.c

---
+++ p2original.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter the value of a numbers to be added\n");
+  scanf("%d",&a);
+  return a;
+}
+
+int add(int a, int b)
+  {
+    int sum;
+    sum=a+b;
+    return sum;
+  }
+
+void output(int a,int b,int sum)
+  {
+   printf("sum of %d and %d is %d",a,b,sum);
+ }
+int main()
+{
+  int x,y,c;
+  x=input();
+  y=input();
+  int sum = add(x,y);
+  output(x,y,sum);
+  return 0;
+}
+
